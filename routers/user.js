8d268306731527a9b39ad5ea0c038a33c6479e3a
deepali-02 <deepali93.redekar@gmail.com>
2022-01-27 09:24:52 +0100

    create new endpoint to fetch expenses as per month

--- routers/user.js
+++ routers/user.js
@@ -24,26 +24,29 @@ router.get("/my_expenses/:id", async (req, res) => {
   const currentMonth = currentDate.getMonth() + 1;
   const currentYear = currentDate.getFullYear();
   const firstOfTheMonth = `${currentYear}/${currentMonth}/01`;
-  const endOfTheMonth = `${currentYear}/${currentMonth}/30`;
-  console.log(
-    `date:${currentDate},month: ${currentMonth}, year: ${currentYear}`
-  );
+  const endOfTheMonth = `${currentYear}/${currentMonth}/31`;
+  // // console.log(
+  //   `date:${currentDate},month: ${currentMonth}, year: ${currentYear}`
+  // );
   const currentMonthExpenses = await MyExpenses.findAll({
     where: { date: { [Op.between]: [firstOfTheMonth, endOfTheMonth] } },
   });
   //console.log("All expenses of current month", currentMonthExpenses);
   const currentMonthExpenses1 = await MyExpenses.findAll({
-    where: { date: { [Op.gte]: [firstOfTheMonth] } },
+    where: { date: { [Op.between]: [firstOfTheMonth, endOfTheMonth] } },
   });
-  console.log("All expenses of current month", currentMonthExpenses1);
+  // console.log("All expenses of current month", currentMonthExpenses1);
   try {
     const id = parseInt(req.params.id);
     const expenses = await MyExpenses.findAll({
       include: [Category],
-      where: { userId: id, date: { [Op.gte]: [firstOfTheMonth] } },
+      where: {
+        userId: id,
+        date: { [Op.between]: [firstOfTheMonth, endOfTheMonth] },
+      },
       order: [["date", "DESC"]],
     });
-    // console.log("My expenses:", expenses);
+    console.log("My expenses:", expenses);
     res.send(expenses);
   } catch (error) {
     console.log(error.message);
@@ -55,29 +58,15 @@ router.post("/my_expenses/:id", async (req, res) => {
   if (!amount || !date || !categoryId) {
     return res.status(400).send("Please provide an amount, date and category");
   }
-
   const currentDate = new Date();
   const currentMonth = currentDate.getMonth() + 1;
   const currentYear = currentDate.getFullYear();
   const firstOfTheMonth = `01/${currentMonth}/${currentYear}`;
+  const endOfTheMonth = `31/${currentMonth}/${currentYear}`;
   console.log(
     `date:${currentDate},month: ${currentMonth}, year: ${currentYear}`
   );
-  //   const where = {
-  //     [Op.or]: [{
-  //         from: {
-  //             [Op.between]: [startDate, endDate]
-  //         }
-  //     }, {
-  //         to: {
-  //             [Op.between]: [startDate, endDate]
-  //         }
-  //     }]
-  // };
-  const currentMonthExpenses = await MyExpenses.findAll({
-    where: { date: { from: { [Op.gte]: firstOfTheMonth } } },
-  });
-  console.log("All expenses of current month", currentMonthExpenses);
+
   try {
     const newExpense = await MyExpenses.create({
       amount,
@@ -86,7 +75,10 @@ router.post("/my_expenses/:id", async (req, res) => {
       userId: req.params.id,
     });
     // console.log("New expense", newExpense);
-    res.send(newExpense);
+    const expense = await MyExpenses.findByPk(newExpense.id, {
+      include: [Category],
+    });
+    res.send(expense);
   } catch (error) {
     console.log(error.message);
   }
@@ -97,6 +89,25 @@ router.get("/category", async (req, res) => {
     const category = await Category.findAll();
     console.log("Category table info", category);
     res.send(category);
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+router.get("/my_expenses/month/:month", async (req, res) => {
+  const month = req.params.month;
+  const currentYear = new Date().getFullYear();
+  const startDateToSearch = `${currentYear}/${month}/01`;
+  const endDateToSearch = `${currentYear}/${month}/31`;
+  console.log("month=", month);
+  console.log("date to search=", startDateToSearch);
+  try {
+    // const year = req.params.year;
+    const monthlyExpenses = await MyExpenses.findAll({
+      where: { date: { [Op.between]: [startDateToSearch, endDateToSearch] } },
+    });
+    console.log("Monthly Expenses", monthlyExpenses);
+    res.send(monthlyExpenses);
   } catch (e) {
     console.log(e.message);
   }
